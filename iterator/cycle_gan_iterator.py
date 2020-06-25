054dde181d123614c27d99ee45aef1237277de77
chrislbie <christopher.lueken.winkels@gmail.com>
2020-06-25 18:54:45 +0200

    added weights for losses and load and save method for cyclegan_iterator

--- iterator/cycle_gan_iterator.py
+++ iterator/cycle_gan_iterator.py
@@ -47,15 +47,17 @@ class Iterator(TemplateIterator):
         """This function returns a dictionary with all neccesary losses for the model."""
         
         reconstruction_criterion = get_loss_funct(self.config["losses"]["reconstruction_loss"])
+        rec_weight = self.config["losses"]["reconstruction_weight"]
         adversarial_criterion = get_loss_funct(self.config["losses"]["adversarial_loss"])
+        adv_weight = self.config["losses"]["adversarial_weight"]
         losses = {}
         #########################
         ###  A: cycle sketch  ###
         #########################
         losses["sketch_cycle"] = {}
         # Generator
-        losses["sketch_cycle"]["rec"] = torch.mean( reconstruction_criterion( self.model.output['real_A'], self.model.output['rec_A'] ) )
-        losses["sketch_cycle"]["adv"] = torch.mean( adversarial_criterion( self.model.netD_B(self.model.output['fake_B']).view(-1) , self.real_labels ) )
+        losses["sketch_cycle"]["rec"] = rec_weight * torch.mean( reconstruction_criterion( self.model.output['real_A'], self.model.output['rec_A'] ) )
+        losses["sketch_cycle"]["adv"] = adv_weight * torch.mean( adversarial_criterion( self.model.netD_B(self.model.output['fake_B']).view(-1) , self.real_labels ) )
         
         # Discriminator
         losses["sketch_cycle"]["disc_fake"]  = torch.mean( adversarial_criterion( self.model.netD_B( self.model.output['fake_B'].detach()).view(-1), self.fake_labels ) )  
@@ -67,8 +69,8 @@ class Iterator(TemplateIterator):
         #######################
         losses["face_cycle"] = {}
         # Generator
-        losses["face_cycle"]["rec"] = torch.mean( reconstruction_criterion( self.model.output['real_B'], self.model.output['rec_B'] ) )
-        losses["face_cycle"]["adv"] = torch.mean( adversarial_criterion( self.model.netD_A(self.model.output['fake_A']).view(-1), self.real_labels ) )
+        losses["face_cycle"]["rec"] = rec_weight * torch.mean( reconstruction_criterion( self.model.output['real_B'], self.model.output['rec_B'] ) )
+        losses["face_cycle"]["adv"] = adv_weight * torch.mean( adversarial_criterion( self.model.netD_A(self.model.output['fake_A']).view(-1), self.real_labels ) )
         
         # Discriminator
         losses["face_cycle"]["disc_fake"]  = torch.mean( adversarial_criterion( self.model.netD_A( self.model.output['fake_A'].detach()).view(-1), self.fake_labels ) )
@@ -215,3 +217,30 @@ class Iterator(TemplateIterator):
                 return lr, amp
         else:
             return self.config["learning_rate"], 1
+
+    def save(self, checkpoint_path):
+        '''
+        'sketch_decoder' refers to the decoder of the face2sketch network
+                        and vice versa. 
+        '''
+        state = {}
+        state['sketch_encoder'] = self.model.netG_A.enc.state_dict()
+        state['sketch_decoder'] = self.model.netG_B.dec.state_dict()
+        state['sketch_discriminator'] = self.model.netD_A.state_dict()
+        state['face_encoder'] = self.model.netG_B.enc.state_dict()
+        state['face_decoder'] = self.model.netG_A.dec.state_dict()
+        state['face_dicriminator'] = self.model.netD_B.state_dict()
+        state['optimizer_G'] = self.optimizer_G.state_dict()
+        state['optimizer_D'] = self.optimizer_D.state_dict()
+        torch.save(state, checkpoint_path)
+
+    def load(self, checkpoint_path):
+        state = torch.load(path)
+        self.model.netG_A.enc.load_state_dict(state['sketch_encoder'])
+        self.model.netG_B.dec.load_state_dict(state['sketch_decoder'])
+        self.model.netD_A.load_state_dict(state['sketch_discriminator'])
+        self.model.netG_B.enc.load_state_dict(state['face_encoder'])
+        self.model.netG_A.dec.load_state_dict(state['face_decoder'])
+        self.model.netD_B.load_state_dict(state['face_discriminator'])
+        self.optimizer_G.load_state_dict(state['optimizer_G'])
+        self.optimizer_D.load_state_dict(state['optimizer_D'])
