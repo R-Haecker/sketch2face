21999c971c7859f4d89dbbba4328ef85e3c4d57f
Richard Haecker <rhaecker@compgpu2.iwr.uni-heidelberg.de>
2020-07-02 14:20:30 +0200

    minor changes

--- model/util.py
+++ model/util.py
@@ -71,8 +71,9 @@ def test_config(config):
     assert "reconstruction_loss" in config["losses"], "The config must contain and define a Loss function for image reconstruction. possibilities:{'L1','L2'or'MSE'}."
     assert "learning_rate" in config, "The config must contain and define a the learning rate."
     if "optimization" in config and "D_accuracy" in config["optimization"]:
-        assert type(config["optimization"]["D_accuracy"]) == list, "D_accuracy has to be a list for discriminator_A and discriminator_B"
-    
+        if config["model_type"] == "sketch2face":
+            assert type(config["optimization"]["D_accuracy"]) == list, "D_accuracy has to be a list for model_type: 'sketch2face' for discriminator_A and discriminator_B"
+        
 def get_act_func(config, logger):
     """This function retruns the specified activation function from the config."""
 
